87519af41bb286ff08feb1f575c50c00d335bfd3
Jeremy Williamson <jeremy.williamson83@gmail.com>
2023-08-13 14:57:59 -0400

    added shuffling

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from PIL import Image
+import random
 
 IMAGE_PIXEL_HEIGHT = 1033
 IMAGE_PIXEL_WIDTH = 947
-
-print("vertical square size(pixels): {0}".format(IMAGE_PIXEL_HEIGHT/5))
-print("horizontal square size(pixels): {0}".format(IMAGE_PIXEL_WIDTH/5))
-
 
 # Open the image
 im = Image.open("images/cat_cropped.jpg")
@@ -17,6 +14,8 @@ width, height = im.size
 tile_width = width // 5
 tile_height = height // 5
 
+tiles = []
+
 # Loop through each tile
 for i in range(5):
     for j in range(5):
@@ -27,6 +26,26 @@ for i in range(5):
         lower = upper + tile_height
         tile = im.crop((left, upper, right, lower))
 
-        # Save the tile as a new image
-        filename = f"tile_{i}_{j}.jpg"
-        tile.save(filename)
+        # Save the tile to the tiles list
+        tiles.append(tile)
+
+# Shuffle the tiles
+random.shuffle(tiles)
+
+# Create a new blank canvas to place the shuffled tiles
+new_image = Image.new("RGB", (width, height))
+
+# Place the shuffled tiles on the canvas
+for i in range(5):
+    for j in range(5):
+        # Get the next tile from the shuffled list
+        tile = tiles.pop(0)
+
+        # Define where to paste the tile on the canvas
+        left = j * tile_width
+        upper = i * tile_height
+
+        new_image.paste(tile, (left, upper))
+
+# Save the new image
+new_image.save("shuffled_cat.jpg")
