8c264f522a9b2befacfc035ede964f8eb629a741
Jeremy Williamson <jeremy.williamson83@gmail.com>
2023-08-13 15:22:10 -0400

    made the gif

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from PIL import Image
 import random
 
-IMAGE_PIXEL_HEIGHT = 1033
-IMAGE_PIXEL_WIDTH = 947
-
 # Open the image
 im = Image.open("images/cat_cropped.jpg")
 
@@ -14,38 +11,50 @@ width, height = im.size
 tile_width = width // 5
 tile_height = height // 5
 
-tiles = []
+# List to hold all shuffled images for gif creation
+shuffled_images = []
 
-# Loop through each tile
-for i in range(5):
-    for j in range(5):
-        # Crop the tile from the image
-        left = j * tile_width
-        upper = i * tile_height
-        right = left + tile_width
-        lower = upper + tile_height
-        tile = im.crop((left, upper, right, lower))
+for _ in range(50):  # Create 50 shuffled images
+    tiles = []
 
-        # Save the tile to the tiles list
-        tiles.append(tile)
+    # Loop through each tile
+    for i in range(5):
+        for j in range(5):
+            # Crop the tile from the image
+            left = j * tile_width
+            upper = i * tile_height
+            right = left + tile_width
+            lower = upper + tile_height
+            tile = im.crop((left, upper, right, lower))
 
-# Shuffle the tiles
-random.shuffle(tiles)
+            # Save the tile to the tiles list
+            if not tile.getbbox():
+                continue
 
-# Create a new blank canvas to place the shuffled tiles
-new_image = Image.new("RGB", (width, height))
+            tiles.append(tile)
 
-# Place the shuffled tiles on the canvas
-for i in range(5):
-    for j in range(5):
-        # Get the next tile from the shuffled list
-        tile = tiles.pop(0)
+    # Shuffle the tiles
+    random.shuffle(tiles)
 
-        # Define where to paste the tile on the canvas
-        left = j * tile_width
-        upper = i * tile_height
+    # Create a new blank canvas to place the shuffled tiles
+    new_image = Image.new("RGB", (width, height))
 
-        new_image.paste(tile, (left, upper))
+    # Place the shuffled tiles on the canvas
+    for i in range(5):
+        for j in range(5):
+            # Get the next tile from the shuffled list
+            tile = tiles.pop(0)
 
-# Save the new image
-new_image.save("shuffled_cat.jpg")
+            # Define where to paste the tile on the canvas
+            left = j * tile_width
+            upper = i * tile_height
+
+            new_image.paste(tile, (left, upper))
+
+    new_image.save(f"debug_image_{_}.jpg")
+
+    shuffled_images.append(new_image)
+
+# Save the shuffled images as an animated gif
+shuffled_images[0].save("shuffled_cat_animation.gif",
+                        save_all=True, append_images=shuffled_images[1:], optimize=False, duration=100, loop=0)
